08fcfaf3ed0f60e98653f89d3878ae609cfd0ced
Alex <sasha-a@ukr.net>
2020-05-10 22:50:05 +0300

    add interactive random char and error

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,10 +1,53 @@
 import React, {Component} from 'react';
 // import './randomChar.css';
 import styled from 'styled-components';
+import gotService from '../../services/gotService';
+import Spinner from '../spinner';
+import ErrorMessage from '../errorMessage';
 
 export default class RandomChar extends Component {
 
+    constructor() {
+        super();
+        this.updateChar();
+    }
+
+    gotService = new gotService();
+    state = {
+        char: {},
+        loading: true,
+        error: false
+    }
+
+    onCharLoaded = (char) => {
+        this.setState({
+            char,
+            loading: false
+        })
+    }
+
+    onError = (err) => {
+        this.setState({
+            error: true,
+            loading: false
+        })
+    }
+
+    updateChar() {
+        const id = 565656565656;
+        // const id = Math.floor(Math.random()*140 + 25); // Диапазон с 25 по 140
+        this.gotService.getCharacter(id)
+            .then(this.onCharLoaded)
+            .catch(this.onError)
+    }
+
     render() {
+
+        const { char, loading, error } = this.state;
+
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? <View char={char}/> : null;
 
         const RandomBlock = styled.div`
             background-color: #fff;
@@ -15,69 +58,86 @@ export default class RandomChar extends Component {
                 margin-bottom: 20px;
                 text-align: center;
             }
+            img {
+                width: 100%;
+            }
         `
-        const ListGroupUl = styled.ul`
-            display: flex;
-            flex-direction: column;
-            padding-left: 0;
-            margin-bottom: 0;
-            
-            li {
-                border-right-width: 0 !important;
-                border-left-width: 0 !important;
-                border-radius: 0 !important;
-                position: relative;
-                display: block;
-                padding: 0.75rem 1.25rem;
-                background-color: #fff;
-                border: 1px solid rgba(0, 0, 0, 0.125);
-                display: flex !important;
-                justify-content: space-between !important;
-            }
-
-            li:first-child {
-                border-top-width: 0;
-                border-bottom-width: 0;
-            }
-
-            li:nth-child(2) {
-                border-bottom-width: 0;
-            }
-            
-            li:last-child {
-                border-bottom-width: 0;
-                border-top-width: 0;
-            }
-        `;
-
-        const Term = styled.span`
-            font-weight: bold;
-        `;
 
         return (
             <RandomBlock>
-                <h4>Random Character: John</h4>
-                <ListGroupUl>
-                    <li>
-                        <Term>Gender </Term>
-                        <span>male</span>
-                    </li>
-                    <li>
-                        <Term>Born </Term>
-                        <span>11.03.1039</span>
-                    </li>
-                    <li>
-                        <Term>Died </Term>
-                        <span>13.09.1089</span>
-                    </li>
-                    <li>
-                        <Term>Culture </Term>
-                        <span>Anarchy</span>
-                    </li>
-                </ListGroupUl>
+                {errorMessage}
+                {spinner}
+                {content}
             </RandomBlock>
         );
     }
+}
+
+const View = ({char}) => {
+    const {name, gender, born, died, culture} = char;
+
+    const ListGroupUl = styled.ul`
+    display: flex;
+    flex-direction: column;
+    padding-left: 0;
+    margin-bottom: 0;
+    
+    li {
+        border-right-width: 0 !important;
+        border-left-width: 0 !important;
+        border-radius: 0 !important;
+        position: relative;
+        display: block;
+        padding: 0.75rem 1.25rem;
+        background-color: #fff;
+        border: 1px solid rgba(0, 0, 0, 0.125);
+        display: flex !important;
+        justify-content: space-between !important;
+    }
+
+    li:first-child {
+        border-top-width: 0;
+        border-bottom-width: 0;
+    }
+
+    li:nth-child(2) {
+        border-bottom-width: 0;
+    }
+    
+    li:last-child {
+        border-bottom-width: 0;
+        border-top-width: 0;
+    }
+`;
+
+const Term = styled.span`
+    font-weight: bold;
+`;
+
+
+    return (
+        <>
+            <h4>Random Character: {name}</h4>
+                <ListGroupUl>
+                    <li>
+                        <Term>Gender </Term>
+                        <span>{gender}</span>
+                    </li>
+                    <li>
+                        <Term>Born</Term>
+                        <span>{born}</span>
+                    </li>
+                    <li>
+                        <Term>Died</Term>
+                        <span>{died}</span>
+                    </li>
+                    <li>
+                        <Term>Culture</Term>
+                        <span>{culture}</span>
+                    </li>
+                </ListGroupUl>
+        </>
+    )
 }
 
 // // Исходный код
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './spinner.css';
+
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-spinner-1sskhfsjf7r">
+            <div class="ldio-kh53dpumj1i">
+                <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner;
+
+
